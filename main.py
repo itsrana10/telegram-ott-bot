41ebf21af610ad76e4b0146464be6b40750cc553
itsrana10 <itsr1050@gmail.com>
2026-06-07 14:47:18 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,29 @@
 import os
-from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import Application, CommandHandler, ContextTypes
+from telegram import Update
+from telegram.ext import (
+    Application,
+    CommandHandler,
+    MessageHandler,
+    ContextTypes,
+    filters,
+)
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    keyboard = [
-        [InlineKeyboardButton("🎬 Movie 1", url="https://t.me/CineBox_BD")]
-    ]
-
     await update.message.reply_text(
-        "Select a movie:",
-        reply_markup=InlineKeyboardMarkup(keyboard)
+        "ভিডিও আমাকে পাঠাও, আমি FILE_ID দেখাব।"
     )
 
+async def get_video(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    if update.message.video:
+        await update.message.reply_text(
+            f"FILE_ID:\n{update.message.video.file_id}"
+        )
+
 app = Application.builder().token(BOT_TOKEN).build()
+
 app.add_handler(CommandHandler("start", start))
+app.add_handler(MessageHandler(filters.VIDEO, get_video))
+
 app.run_polling()
