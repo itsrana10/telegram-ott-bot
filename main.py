b01dbbff4f8c8ebae52ac09625508826f4881223
itsrana10 <itsr1050@gmail.com>
2026-06-07 09:42:22 +0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+from telegram import Update
+from telegram.ext import Application, CommandHandler, ContextTypes
+
+BOT_TOKEN = "PASTE_YOUR_BOT_TOKEN_HERE"
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("🎬 Welcome to Movie Bot!")
+
+app = Application.builder().token(BOT_TOKEN).build()
+
+app.add_handler(CommandHandler("start", start))
+
+app.run_polling()
