7591d9ab9a298e1a9acaeb23c25dc59819b0b87d
itsrana10 <itsr1050@gmail.com>
2026-06-08 14:28:56 +0600

    Update main.py

--- main.py
+++ main.py
@@ -3,31 +3,34 @@ from telegram import Update
 from telegram.ext import (
     Application,
     CommandHandler,
+    MessageHandler,
     ContextTypes,
+    filters,
 )
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
-VIDEO1 = "BAACAgQAAyEFAASsX1-0AAMQaiUugm7SJt-edUnbvW-7UxHoDhoAAkseAAL3PwFRowNLEaf6ZNI7BA"
+ADMIN_ID = 7890690659
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-
-    if len(context.args) > 0:
-
-        if context.args[0] == "movie1":
-            await context.bot.send_video(
-                chat_id=update.effective_user.id,
-                video=VIDEO1,
-                caption="🎬 Movie 1"
-            )
-            return
-
     await update.message.reply_text(
-        f"Welcome to CineBox BD 🎬\n\nYour ID: {update.effective_user.id}"
+        f"CineBox BD\n\nYour ID: {update.effective_user.id}"
     )
+
+async def get_file_id(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    if update.effective_user.id != ADMIN_ID:
+        return
+
+    video = update.message.video
+
+    if video:
+        await update.message.reply_text(
+            f"📁 FILE ID:\n\n{video.file_id}"
+        )
 
 app = Application.builder().token(BOT_TOKEN).build()
 
 app.add_handler(CommandHandler("start", start))
+app.add_handler(MessageHandler(filters.VIDEO, get_file_id))
 
 app.run_polling()
